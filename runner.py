f207f914205ebdf650f4a12208c3c22586273332
simonbaev <simonbaev@gmail.com>
2014-12-25 23:08:52 -0500

    Card validation code

--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import Adafruit_CharLCD as LCD
 import serial
+import pycard
+import re
 import threading, sys, time
 
 
@@ -46,11 +48,10 @@ class runner(threading.Thread):
         offsetLength = 0
         progTemp = '>>>'
         self.lcd.message(' Slide the card ')
-        time.sleep(2)
         while True:
             if self.ser.inWaiting() > 0:
-                time.sleep(1)
-                print self.ser.read(self.ser.inWaiting())
+                time.sleep(0.5)
+                cardDataString = self.ser.read(self.ser.inWaiting()).lstrip('%B').rsplit('?')[0]
                 break
             else:
                 progString = "%-14s" % ((' ' * offsetLength) + progTemp)
@@ -59,7 +60,29 @@ class runner(threading.Thread):
                     self.lcd.write8(ord(c), True)
                 offsetLength = (offsetLength + 1) % (14 - len(progTemp) + 1)
                 time.sleep(0.1)
-
+        cardDataTokens = cardDataString.split('^',3)
+        #-- Card number
+        cardNumber = re.sub('[^0-9]','',cardDataTokens[0].strip())
+        #-- Card owner
+        try:
+            cardOwner = cardDataTokens[1].strip().replace('/',' ')
+        except:
+            cardOwner = 'N/A'
+        #-- Card expiration date
+        try:
+            cardExpDate = cardDataTokens[2].strip()[0:4]
+        except:
+            cardExpDate = 'N/A'
+        #-- Card data validation
+        try:
+            cardObject = pycard.Card(number=cardNumber,month=int(cardExpDate[2:]),year=int('20'+cardExpDate[0:2]),cvc='')
+            cardValid = cardObject.is_valid
+            cardType = cardObject.brand.capitalize()
+        except:
+            cardType = 'N/A'
+            cardValid = False
+        #-- Report
+        print '%-20s%s\n%-20s%s\n%-20s%s\n%-20s%s\n%-20s%s\n' % ('Number:',cardNumber,'Owner:',cardOwner,'Expiration date:',cardExpDate,'Type',cardType,'Valid:',cardValid)
 
 if __name__ == '__main__':
     runner().start()
